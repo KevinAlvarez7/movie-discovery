4fd754732c6326697488431a893abd4bbbe8616e
Kevin Alvarez <kevinalvarez373@gmail.com>
2025-01-01 06:18:17 +0800

    change

--- src/components/MovieCards/MovieCard.tsx
+++ src/components/MovieCards/MovieCard.tsx
@@ -5,6 +5,7 @@ import React, { JSX } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import ReactStars from 'react-stars';
+// In MovieCard.tsx, update to:
 import { NoiseBackground } from '../../components/ui/NoiseBackground';
 import TornContainer from '../../components/ui/TornContainer';
 
