c23e53e9b1d46460106a9b08dee0498edab4ed39
Kevin Alvarez <kevinalvarez373@gmail.com>
2025-01-08 19:28:53 +0800

    fixed deployment issues for db

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 // Create a singleton instance of PrismaClient
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
